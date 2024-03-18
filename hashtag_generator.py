d76761093950eaccde825c88a34dce0cccaeb5fc
Miguel Correa <mctesterwork@gmail.com>
2024-03-18 12:59:35 -0500

    adding customtkinter library for gui

--- hashtag_generator.py
+++ hashtag_generator.py
@@ -1,9 +1,24 @@
 import customtkinter
 import chat_settings as chat_settings
 
-
 # TODO: Add GUI, with an input for the number of hashtags and text input for topic,
-response = chat_settings.getHashtags(15, 'quality assurance')
-print(response)
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("400x240")
+
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+app.mainloop()
+
+
+#response = chat_settings.getHashtags(15, 'quality assurance')
+#print(response)
 
 
